ec14691fdb3838aa4d1985fb1c71d7cf63af8aa8
pavel <pavelzavalnyuk11@gmail.com>
2023-04-10 15:53:54 +0300

    fix: pathes

--- public/js/next_previous_buttons.js
+++ public/js/next_previous_buttons.js
@@ -16,5 +16,5 @@ nextButton.addEventListener('click', () => {
 });
 
 function redirect(page) {
-  window.location.href = `/products/page/${page}`;
+  window.location.href = `https://crbpavel.onrender.com/products/page/${page}`;
 }
