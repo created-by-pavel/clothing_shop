3d521db85ec38b191224e417656c25e260e0f3ec
pavel <pavelzavalnyuk11@gmail.com>
2024-05-23 10:02:24 +0300

    fix: menu section

--- public/js/menu.js
+++ public/js/menu.js
@@ -10,3 +10,18 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
     toggle_menu.classList.remove("active");
     nav_menu.classList.remove("active");
 }))
+
+document.addEventListener("DOMContentLoaded", function() {
+    const userId = localStorage.getItem('userId');
+    const profileLink = document.getElementById("profile-link");
+    const loginLi = document.getElementById("login-li");
+    const profileLi = document.getElementById("profile-li");
+    if (userId) {
+        loginLi.style.display = "none";
+        profileLi.style.display = "block";
+        profileLink.href = `https://clothing-shop-5n2c.onrender.com/user/profile/${encodeURIComponent(userId)}`;
+    } else {
+        loginLi.style.display = "block";
+        profileLi.style.display = "none";
+    }
+});
